00ebb81e552291fa143906b233631dae7a2d5bda
Hassan Raza <hassan2056764@gmail.com>
2025-04-12 05:45:05 +0500

    first route

--- src/app.js
+++ src/app.js
@@ -13,5 +13,12 @@ app.use(express.json({ limit: "15kb" }))
 app.use(express.urlencoded({ extended: true, limit: "15kb" }))
 app.use(express.static("public"))
 
+//routes import
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/user/register
 
 export { app }
