ce60aa14e3e7dc334d84c618a755e39c95a46f6d
Hassan Raza <hassan2056764@gmail.com>
2025-04-14 01:36:20 +0500

    add register controller and bug fixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middlewares.js";
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.field([
+        {
+            name: "avatar",  //when writing the front use same for connection with frontend and backend
+            maxCount = 1
+        },
+        {
+            name: "coverImage",
+            maxcount:
+        } //middleware injection on whichever method is executing
+    ]),
+    registerUser)
 
 export default router
 
